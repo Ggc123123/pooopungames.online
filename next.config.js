6cd0d3f7390b6f3ccd9b091c8689388a6bcd4332
Ggc123123 <18469104604@163.com>
2025-03-25 00:52:09 +0800

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,47 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: [
-      'placehold.co',
-      'example.com',
       'www.onlinegames.io',
       'onlinegames.io',
-      'img.onlinegames.io',
-      'assets.onlinegames.io',
-      'cdn.onlinegames.io',
-      'images.onlinegames.io',
-      'media.onlinegames.io',
-      'static.onlinegames.io',
-      'www.crazygames.com',
-      'images.crazygames.com',
-      'cdn.crazygames.com',
-      'gameforge.com',
-      'cdn.gameforge.com',
-      'images.gameforge.com',
+      'cloud.onlinegames.io',
       'img.gamedistribution.com',
-      'media.gamedistribution.com',
-      'static.gamedistribution.com',
-      'assets.gamedistribution.com'
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.onlinegames.io'
-      },
-      {
-        protocol: 'https',
-        hostname: '**.crazygames.com'
-      },
-      {
-        protocol: 'https',
-        hostname: '**.gamedistribution.com'
-      },
-      {
-        protocol: 'https',
-        hostname: '**.gameforge.com'
-      }
+      'imgs2.dab3games.com',
+      'games.poki.com'
     ]
-  }
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/pooopungames.online' : '',
 }
 
 module.exports = nextConfig 
