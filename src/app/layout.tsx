dfe4f4d7c6497007ce8c749034615f90ee983239
Ggc123123 <18469104604@163.com>
2025-03-25 22:33:17 +0800

    添加：Google Analytics 跟踪代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -50,6 +51,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0X437TDZ6V"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-0X437TDZ6V');
+          `}
+        </Script>
+        {/* Cloudflare Web Analytics */}
+        <Script id="cloudflare-web-analytics" strategy="afterInteractive">
+          {`
+            var beacon = new Image();
+            beacon.src = "https://cloudflareinsights.com/cdn-cgi/beacon/";
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
